0c5066311ee1a33ebd518ca12303ca021bb3e2d0
Yoruhi <alexandre.farnier@telecomnancy.net>
2024-12-01 19:21:57 +0100

    Day 01 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,47 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val col1 = ArrayList<Int>()
+        val col2 = ArrayList<Int>()
+
+        // create two list to save the two column
+        for (line in input){
+            col1.add(line.split("   ")[0].toInt())
+            col2.add(line.split("   ")[1].toInt())
+        }
+
+        // sort the two list created
+        col1.sort()
+        col2.sort()
+
+        var result = 0
+        for( i in col1.indices){
+            result += abs(col1[i]-col2[i])
+        }
+
+        return result
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val col1 = ArrayList<Int>()
+        val col2 = ArrayList<Int>()
+
+        // create two list to save the two column
+        for (line in input){
+            col1.add(line.split("   ")[0].toInt())
+            col2.add(line.split("   ")[1].toInt())
+        }
+
+        var result = 0
+
+        for (elem in col1)
+        {
+            result += elem *  col2.count { it == elem }
+        }
+
+        return result
     }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
